3b937f3c21f86b370f81bb85a9f8114bfbf64a72
Chris Emberley (WinMister332) <chris.emberley1001@gmail.com>
2018-02-27 19:56:54 -0600

    Add files via upload

---
+++ EasyTabs-master/EasyTabs-master/TestApp/TestApp.cs
@@ -0,0 +1,27 @@
+﻿using EasyTabs;
+
+namespace TestApp
+{
+	public partial class TestApp : TitleBarTabs
+    {
+        public TestApp()
+        {
+            InitializeComponent();
+
+            AeroPeekEnabled = true;
+            TabRenderer = new ChromeTabRenderer(this);
+            Icon = Resources.DefaultIcon;
+        }
+
+        public override TitleBarTab CreateTab()
+        {
+            return new TitleBarTab(this)
+            {
+                Content = new TabWindow
+                {
+                    Text = "New Tab"
+                }
+            };
+        }
+    }
+}
